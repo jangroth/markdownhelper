d4da4b426d9309d5477952bcc0fdca8ecc214241
Jan Groth <jan.groth.de@gmail.com>
2019-10-01 17:36:59 +1000

    Fix broken tests

--- tests/markdown_helper_tests.py
+++ tests/markdown_helper_tests.py
@@ -27,10 +27,10 @@ WITH_DEBUG = True
 def test_should_equal_and_not_equal():
     md1 = MarkdownLine('a')
     md2 = MarkdownLine('a')
-    md3 = MarkdownLine('# b', [1])
+    md3 = MarkdownLine('# b', (1,))
     assert md1 == md2
     assert md1 != md3
-    assert str(md3) == '[2] - # b'
+    assert str(md3) == '(2,):# b'
 
 
 def test_should_create_anchor_name_from_index(mdl):
@@ -49,35 +49,35 @@ def test_should_get_header_level_from_line(mdl):
 
 
 def test_calculate_new_index(mdl):
-    assert mdl._generate_index([], 'test') == []
-    assert mdl._generate_index([1], 'test') == []
+    assert mdl._generate_index((), 'test') == ()
+    assert mdl._generate_index((1,), 'test') == ()
 
-    assert mdl._generate_index([], '# test') == [1]
-    assert mdl._generate_index([2], '## test') == [2, 1]
+    assert mdl._generate_index((), '# test') == (1,)
+    assert mdl._generate_index((2,), '## test') == (2, 1)
 
-    assert mdl._generate_index([1], '# test') == [2]
-    assert mdl._generate_index([1, 2], '## test') == [1, 3]
+    assert mdl._generate_index((1,), '# test') == (2,)
+    assert mdl._generate_index((1, 2), '## test') == (1, 3)
 
-    assert mdl._generate_index([1, 2], '# test') == [2]
-    assert mdl._generate_index([1, 2, 3], '## test') == [1, 3]
+    assert mdl._generate_index((1, 2), '# test') == (2,)
+    assert mdl._generate_index((1, 2, 3), '## test') == (1, 3)
 
 
 def test_should_render_link_to_top():
-    assert MarkdownLine('a').link_to_top == '[▲](#top)'
+    assert MarkdownLine('a').link_to_top == '[↖](#top)'
 
 
 def test_should_render_link_to_previous():
-    assert MarkdownLine('### a', [1, 2]).link_to_previous == '[◄](#1_2)'
-    assert MarkdownLine('## a', [1, 2]).link_to_previous == '[◄](#1_2)'
+    assert MarkdownLine('### a', (1, 2)).link_to_previous == '[↑](#1_2)'
+    assert MarkdownLine('## a', (1, 2)).link_to_previous == '[↑](#1_2)'
 
 
 def test_should_render_to_markdown():
     assert MarkdownLine('a').to_markdown() == 'a'
     assert MarkdownLine('a').to_markdown(WITH_ANCHOR, WITH_DEBUG) == 'a'
-    assert MarkdownLine('# 1').to_markdown() == '# 1'
-    assert MarkdownLine('# 1').to_markdown(WITHOUT_ANCHOR, WITH_DEBUG) == '[1] - # 1'
-    assert MarkdownLine('# 1').to_markdown(WITH_ANCHOR, WITH_DEBUG) == '\n[▲](#top)\n<a name="1"></a>\n[1] - # 1'
-    assert MarkdownLine('# 1').to_markdown(WITH_ANCHOR) == '\n[▲](#top)\n<a name="1"></a>\n# 1'
+    assert MarkdownLine('# 1').to_markdown() == '# [↖](#top)[↑](#)[↓](#) 1'
+    assert MarkdownLine('# 1').to_markdown(WITHOUT_ANCHOR, WITH_DEBUG) == '# [↖](#top)[↑](#)[↓](#)(1,) -  1'
+    assert MarkdownLine('# 1').to_markdown(WITH_ANCHOR, WITH_DEBUG) == '<a name="1"></a>\n# [↖](#top)[↑](#)[↓](#)(1,) -  1'
+    assert MarkdownLine('# 1').to_markdown(WITH_ANCHOR) == '<a name="1"></a>\n# [↖](#top)[↑](#)[↓](#) 1'
 
 
 def test_should_render_to_toc_entry():
@@ -102,4 +102,4 @@ def test_cleanse_multiple_lines(mdd):
 
 def test_convert_multiple_lines(mdd):
     md_data = [(md_line.index, md_line.line) for md_line in mdd._cleansed_to_md(['one', '# two', 'three', '# four'])]
-    assert md_data == [([], 'one'), ([1], '# two'), ([], 'three'), ([2], '# four')]
+    assert md_data == [((), 'one'), ((1,), '# two'), ((), 'three'), ((2,), '# four')]
